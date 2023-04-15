5bc4febdcd1bffcb1c88d477511c81ea688ac90a
Nguyen3006-IT <130127124+Nguyen3006-IT@users.noreply.github.com>
2023-04-15 13:22:06 +0700

    dòng chữ màu vô hạng

---
+++ chữ_vô_tận.py
@@ -0,0 +1,21 @@
+ # ngôn ngữ Python
+ # dòng chữ màu vô hạng
+ 
+ import random
+import datetime
+import time
+
+color = ['\033[30m', '\033[31m',
+         '\033[32m', '\033[33m',
+         '\033[34m', '\033[35m',
+         '\033[36m', '\033[37m',
+         '\033[38m', '\033[39m',]
+              
+N = datetime.date(2021, 6, 13)
+ 
+while True:
+        i= random.randint(0, 9)
+        print(color[i] + N.strftime(" %d-%m-%y"), 
+        ': Anh yêu em vô cực - Anh yêu em vô cực - Anh yêu em vô cực - Anh yêu em vô cực',sep="")
+        time.sleep(0.01)
+        N += datetime.timedelta(days = 1)
