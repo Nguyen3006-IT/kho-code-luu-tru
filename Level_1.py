90a69205e80171e121a0dc665a24396fc4efc70c
Nguyen3006-IT <130127124+Nguyen3006-IT@users.noreply.github.com>
2023-04-24 10:50:19 +0700

    Update Level_1.py

--- Level_1.py
+++ Level_1.py
@@ -25,3 +25,14 @@ print (fact(x))
 --------------------------------------------------------------------------------------------------------
 
 """Bài 3: """ 
+""" Với số nguyên n nhất định, hãy viết chương trình để tạo ra một dictionary chứa (i, i*i) 
+như là số nguyên từ 1 đến n (bao gồm cả 1 và n) sau đó in ra dictionary này."""
+
+n=int(input("Nhập vào một số:"))
+d = dict()
+for i in range(1,n+1):
+    d[i] = i*i # vì ta cho d = dict() nên sẽ có kiểu {"i": "i*i",...}
+
+print (d)
+--------------------------------------------------------------------------------------------------------
+
